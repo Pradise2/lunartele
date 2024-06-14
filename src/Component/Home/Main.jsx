2c926ece2cbf698e06118aa2c3df50c0ee5a9d3f
Pradise2 <165901189+Pradise2@users.noreply.github.com>
2024-06-14 09:24:31 +0100

    Initial commit

--- src/Component/Home/Main.jsx
+++ src/Component/Home/Main.jsx
@@ -72,7 +72,7 @@ const Main = () => {
   return (
     <div className="bg-zinc-900 text-white flex flex-col items-center p-0 space-y-4 overflow-hidden">
       <div className="p-0 rounded-lg text-center w-full max-w-md">
-        <p className="text-zinc-400 font-bold">Lunar Tokens</p>
+        <p className="text-zinc-400 font-bold">Lunar Token</p>
         <p className="text-4xl font-bold">
           {(c4Count + d2Claimed).toFixed(2)} <span className="text-purple-400">lunar</span>
         </p>
