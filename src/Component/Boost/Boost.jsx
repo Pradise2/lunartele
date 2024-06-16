65303b6340013ea7a86485207900aaf9bf580f5b
Pradise2 <165901189+Pradise2@users.noreply.github.com>
2024-06-16 14:56:21 +0100

    Initial commit

--- src/Component/Boost/Boost.jsx
+++ src/Component/Boost/Boost.jsx
@@ -2,22 +2,7 @@ import Footer from '../Others/Footer'
 import React, { useEffect, useState } from 'react';
 import { initializeApp } from 'firebase/app';
 import { getFirestore, doc, setDoc } from 'firebase/firestore';
-
-// Firebase Configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyAURFbyDHkq626UusPHMijpxmcUOOl5-Tw",
-    authDomain: "test-f326f.firebaseapp.com",
-    projectId: "test-f326f",
-    storageBucket: "test-f326f.appspot.com",
-    messagingSenderId: "626801402709",
-    appId: "1:626801402709:web:d3653b964333a0de6845dc",
-    measurementId: "G-517PH4LM9K",
-    databaseURL: "https://test-f326f-default-rtdb.firebaseio.com/" // You might need this if you still need to use the Realtime Database
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+import { db } from '../../firebase';
 
 const Boost = () => {
   const [userData, setUserData] = useState(null);
