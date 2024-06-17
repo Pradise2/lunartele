5471e0ac2098f4a5f7f73b55a8451007e0e82fb1
Pradise2 <165901189+Pradise2@users.noreply.github.com>
2024-06-17 23:54:09 +0100

    Initial commit

--- src/Component/Home/Main.jsx
+++ src/Component/Home/Main.jsx
@@ -1,3 +1,4 @@
+
 import tapps from '../../assets/tapps.png';
 import React, { useState, useEffect, useRef } from "react";
 import Footer from '../Others/Footer';
@@ -7,17 +8,16 @@ import { doc, setDoc, getDoc, updateDoc } from "firebase/firestore";
 const Main = () => {
   const [userId, setUserId] = useState(null);
   const [tapLeft, setTapLeft] = useState(1000);
-  const [tapTime, setTapTime] = useState(4*60*60);
+  const [tapTime, setTapTime] = useState(4 * 60 * 60);
   const [taps, setTaps] = useState(0);
   const [farmTime, setFarmTime] = useState(30);
   const [farm, setFarm] = useState(0);
   const [farmClaimed, setFarmClaimed] = useState(0);
-  const [isClaimClicked, setIsClaimClicked] = useState(false);
   const [totalBal, setTotalBal] = useState(0);
   const [firstname, setFirstName] = useState(null);
   const [userExists, setUserExists] = useState(false);
   const farmIntervalRef = useRef(null);
-  const [isFarmActive, setIsFarmActive] = useState(false); // Track if the farm is active
+  const [isFarmActive, setIsFarmActive] = useState(false);
 
   window.Telegram.WebApp.expand();
 
@@ -42,7 +42,7 @@ const Main = () => {
       if (localStorageData) {
         const currentTime = new Date().getTime();
         const elapsedTime = Math.floor((currentTime - localStorageData.lastLoginTime) / 1000);
-        const farmElapsedTime = isClaimClicked ? Math.floor((currentTime - localStorageData.farmStartTime) / 1000) : 0;
+        const farmElapsedTime = isFarmActive ? Math.floor((currentTime - localStorageData.farmStartTime) / 1000) : 0;
 
         setTapLeft(localStorageData.tapLeft);
         setTapTime(Math.max(localStorageData.tapTime - elapsedTime, 0));
@@ -50,9 +50,8 @@ const Main = () => {
         setFarmTime(Math.max(localStorageData.farmTime - farmElapsedTime, 0));
         setFarm(localStorageData.farm + (isFarmActive ? farmElapsedTime * 0.01 : 0));
         setFarmClaimed(localStorageData.farmClaimed);
-        setTotalBal(localStorageData.totalBal);
+        setTotalBal(localStorageData.totalBal + (isFarmActive ? farmElapsedTime * 0.01 : 0));
         setUserExists(true);
-        console.log("Document data loaded from local storage:", localStorageData);
 
         if (isFarmActive && farmTime > 0) {
           startFarmInterval();
@@ -67,15 +66,15 @@ const Main = () => {
         const data = docSnap.data();
         const currentTime = new Date().getTime();
         const elapsedTime = Math.floor((currentTime - data.lastLoginTime) / 1000);
-        const farmElapsedTime = isClaimClicked ? Math.floor((currentTime - data.farmStartTime) / 1000) : 0;
+        const farmElapsedTime = isFarmActive ? Math.floor((currentTime - data.farmStartTime) / 1000) : 0;
 
         setTapLeft(data.tapLeft);
         setTapTime(Math.max(data.tapTime - elapsedTime, 0));
         setTaps(data.taps);
         setFarmTime(Math.max(data.farmTime - farmElapsedTime, 0));
-        setFarm(data.farm + (isFarmActive ? farmElapsedTime * 0.01 : 0));setFarmClaimed(data.farmClaimed);
+        setFarm(data.farm + (isFarmActive ? farmElapsedTime * 0.01 : 0));
         setFarmClaimed(data.farmClaimed);
-        setTotalBal(data.totalBal);
+        setTotalBal(data.totalBal + (isFarmActive ? farmElapsedTime * 0.01 : 0));
         setUserExists(true);
 
         localStorage.setItem(`userData-${userId}`, JSON.stringify(data));
@@ -110,7 +109,7 @@ const Main = () => {
         farm: farm,
         farmClaimed: farmClaimed,
         lastLoginTime: currentTime,
-        farmStartTime:  isFarmActive ? currentTime : null,
+        farmStartTime: isFarmActive ? currentTime : null,
       };
 
       if (userExists) {
@@ -132,13 +131,13 @@ const Main = () => {
       handleSendData();
     }
   }, [userId, firstname, totalBal, tapLeft, tapTime, taps, farmTime, farm, farmClaimed, isFarmActive]);
- 
+
   useEffect(() => {
     const intervalIdC2 = setInterval(() => {
       setTapTime((prevtapTime) => {
         if (prevtapTime <= 0) {
           setTapLeft(1000);
-          return 4*60*60;
+          return 4 * 60 * 60;
         }
         return prevtapTime - 1;
       });
@@ -153,14 +152,14 @@ const Main = () => {
 
   const startFarmInterval = () => {
     farmIntervalRef.current = setInterval(() => {
-      setFarmTime((prevfarmTime) => {
-        if (prevfarmTime <= 0) {
+      setFarmTime((prevFarmTime) => {
+        if (prevFarmTime <= 0) {
           clearInterval(farmIntervalRef.current);
-          setIsFarmActive(false); // Stop the farm
+          setIsFarmActive(false);
           return 0;
         } else {
-          setFarm((prevfarm) => prevfarm + 0.01);
-          return prevfarmTime - 1;
+          setFarm((prevFarm) => prevFarm + 0.01);
+          return prevFarmTime - 1;
         }
       });
     }, 1000);
@@ -173,23 +172,20 @@ const Main = () => {
     }
   };
 
-  
   const handleStartClick = () => {
     if (!isFarmActive) {
-      // Start the farm timer only if it's not already running
-      if (farmTime > 0) { 
+      if (farmTime > 0) {
+        setIsFarmActive
         setIsFarmActive(true);
         startFarmInterval();
       }
-    } else if (farmTime === 0) { // Check for claim condition
-      setFarmClaimed(farmClaimed + farm);
-      setFarm(0);
-      setFarmTime(30); 
+    } else if (farmTime === 0) {
+      setFarmClaimed((prevFarmClaimed) => prevFarmClaimed + farm);
+    setTotalBal((prevTotalBal) => prevTotalBal + farm);
+    setFarm(0);
+      setFarmTime(30);
       setIsFarmActive(false);
       clearInterval(farmIntervalRef.current);
-      
-    // Update totalBal when claim is clicked
-    setTotalBal(totalBal + farm);  
     }
   };
 
@@ -197,8 +193,7 @@ const Main = () => {
     const hours = Math.floor(time / 3600);
     const minutes = Math.floor((time % 3600) / 60);
     const seconds = Math.floor(time % 60);
-    const formattedTime = `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-    return formattedTime;
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
   };
 
   useEffect(() => {
@@ -238,22 +233,21 @@ const Main = () => {
     }
   }, [tapLeft, tapTime, taps, farmTime, farm, farmClaimed, totalBal, isFarmActive]);
 
-
   return (
     <div className="max-h-screen bg-zinc-900 text-white flex flex-col items-center p-0 space-y-4 overflow-hidden">
       <div className="p-2 rounded-lg text-center w-full max-w-md">
-        <p className="p-2 text-zinc-400 font-bold">Lunar Token</p>
+      <p className="p-2 text-zinc-400 font-bold">Lunar Token</p>
         <p className="text-4xl font-bold">
           {(totalBal).toFixed(2)} <span className="text-purple-400">lunar</span>
         </p>
       </div>
       <div className="text-center space-y-2">
         <p className="text-zinc-400">
-          Wont stop! tapTime shows refill, {userId ? `${userId} ` : ''} but the fun won’t flop! <span className="text-yellow-400">👍</span>
+          Won't stop! Tap time shows refill, {userId ? `${userId} ` : ''} but the fun won’t flop! <span className="text-yellow-400">👍</span>
         </p>
         <div className="p-2 flex justify-center space-x-4">
           <div className="bg-purple-800 p-2 rounded-lg flex">
-            <p>{tapLeft} tapLeft</p>
+            <p>{tapLeft} taps left</p>
           </div>
           <div className="bg-yellow-800 p-2 rounded-lg flex items-center space-x-2">
             <span className="material-icons">access_time</span>
@@ -265,7 +259,7 @@ const Main = () => {
         <img
           id="C3"
           src={tapps}
-          alt="lunar Token"
+          alt="Lunar Token"
           className="rounded-full w-24 h-24"
           onClick={handleClickC3}
         />
@@ -285,13 +279,14 @@ const Main = () => {
       <button
         className="mt-6 bg-zinc-700 text-white py-2 px-6 rounded-lg"
         onClick={handleStartClick}
-        disabled={isFarmActive && farmTime > 0} // Changed disable condition
+        disabled={isFarmActive && farmTime > 0} 
       >
         {isFarmActive && farmTime === 0 ? "Claim" : "Start"} 
       </button>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
 
 export default Main;
+
